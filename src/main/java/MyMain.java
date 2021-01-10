07655ea796122af6e1a130eff0a23c0a28124c21
nmakdessi <21nmakdessi@athenian.org>
2021-01-10 20:54:40 +0000

    completed assignment

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,12 +2,27 @@ public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int[] deck = new int[arr.length];
+        for (int i = 0; i < deck.length; i++){
+            if (i%2 == 0){
+                deck[i] = arr[i/2];
+            }
+            else{
+                deck[i] = arr[i/2 + deck.length/2];
+            }
+        }
+        return deck;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+        for (int i = 0; i < arr.length -1; i++){
+            int random = (int)(Math.random() * (arr.length - i - 1)) + i + 1;
+            int temp = arr[i];
+            arr[i] = arr[random];
+            arr[random] = temp;
+        }
+        return arr;
     }
 
 
